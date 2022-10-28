5e5a263e990f6f6602cb0fc804e027b5c4fd0727
Ser-Dom <drpiton@rambler.ru>
2022-10-28 18:27:54 +0300

    практика циклов

--- js/script22.js
+++ js/script22.js
@@ -1,5 +1,6 @@
+"use strict";
 
-const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели", '');
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели", ''); //+превращает строку в число
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -9,12 +10,41 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Один из последних просмотрееных фильмов', ''),
+/* const a = prompt('Один из последних просмотрееных фильмов', ''),
       b = prompt('На сколько оцените его', ''),
       c = prompt('Один из последних просмотрееных фильмов', ''),
-      d = prompt('На сколько оцените его', '');
+      d = prompt('На сколько оцените его', ''); */
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+/* personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d; */
+
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотрееных фильмов', ''),
+          b = prompt('На сколько оцените его', '');
+
+    if (a != null && b !=null && a != '' && b !='' && a.length < 50) {
+
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    } 
+
+}
+
+if(personalMovieDB.count < 10) {
+
+    console.log('Просмотрено довольно мало фильмов');
+
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+
+    console.log('Вы классический зритель');
+
+} else  {
+
+    console.log('Вы киноман');
+    
+}
 
 console.log(personalMovieDB);
