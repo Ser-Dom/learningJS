3e2d11eb7e6522bf699f4d12915796afc2b4de6d
Ser-Dom <drpiton@rambler.ru>
2022-11-22 20:14:54 +0300

    динамическая типизация

--- js/script 26.js
+++ js/script 26.js
@@ -1,21 +1,50 @@
 'use strict';
 
-function hello () {
-    console.log('Hello world');
+//1 превращение в строку
+//1,1
+console.log(typeof(String(null)));
+console.log(typeof(String(4)));
+
+//1,2
+console.log(typeof(5 + ''));
+
+const num = 5;
+
+console.log('https://vk.com/catalog/' + num);
+
+const fontSize = 26 + 'px';
+
+//2 превращение в число 
+//2,1
+console.log(typeof(Number('4')));
+
+//2,2
+console.log(typeof(+'5'));
+
+//2,3
+console.log(typeof(parseInt('15', 10)));
+
+
+let answer = +prompt('Hello', '');
+
+//3 в булиновые значения
+//3,1
+
+// 0, '', null, undefined, NaN это все превращается в false, все остальное true 
+
+let switcher = null;
+
+if (switcher) {
+    console.log('Not Working...');
 }
 
-hello();
-
-function hi() {
-    console.log('Say hi!');
+switcher = 1;
+if (switcher) {
+    console.log('Working...');
 }
 
-hi();
+//3,2
+console.log(typeof(Boolean('4')));
 
-const arr = [1, 14, 4, 30, 54],
-    sorted = arr.sort(compareNum);
-
-function compareNum(a, b) {
-    return a - b;
-}
-console.log(sorted);
+//3,3
+console.log(typeof(!!"756"));
---
+++ js/script 27.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/* let x = 5;
+alert(x++); */
+
+let a = [] + false - null + true;
+console.log(a);
+
+/* let y = 1;
+let x = y = 2;
+alert(x); */
+
+let q = [] + 1 + 2;
+console.log(q);
+console.log(typeof(q));
+
+alert('1'[1]);
+
+/* alert("Infinity"); */
